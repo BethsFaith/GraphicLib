b0cade10ff7899829b07a3905e1644950872d12b
BethsFaith <83585744+BethsFaith@users.noreply.github.com>
2024-03-06 19:29:35 +0300

    Feature/widget-style-classes (#8)
    * Added style classes
    
    * Added getters

--- src/widgets/WidgetBuilder.cpp
+++ src/widgets/WidgetBuilder.cpp
@@ -6,11 +6,19 @@
 
 namespace GraphicLib::Widgets {
     void WidgetBuilder::addWidgetStyle(WidgetType type, const Styles::WidgetStyle::Ptr& style) {
-        styles[type] = style;
+        addWidgetStyle(type, DEFAULT_STYLE, style);
+    }
+
+    void WidgetBuilder::addWidgetStyle(WidgetType type, const std::string& styleClass, const Styles::WidgetStyle::Ptr& style) {
+        styles[std::to_string(type)+styleClass] = style;
     }
 
     Widget::Ptr WidgetBuilder::createWidget(WidgetType type) {
-        return WidgetFactory::create(type, styles[type]);
+        return createWidget(type, DEFAULT_STYLE);
+    }
+
+    Widget::Ptr WidgetBuilder::createWidget(WidgetType type, const std::string& styleClass) {
+        return WidgetFactory::create(type, styles[std::to_string(type)+styleClass]);
     }
 
     void WidgetBuilder::addLayoutStyle(LayoutType type, const Styles::LayoutStyle::Ptr& style) {
@@ -19,5 +27,17 @@ namespace GraphicLib::Widgets {
 
     Layout::Ptr WidgetBuilder::createLayout(LayoutType type) {
         return WidgetFactory::create(type, layoutStyles[type]);
+    }
+
+    Styles::WidgetStyle::Ptr WidgetBuilder::getWidgetStyle(WidgetType type) {
+        return getWidgetStyle(type, DEFAULT_STYLE);
+    }
+
+    Styles::WidgetStyle::Ptr WidgetBuilder::getWidgetStyle(WidgetType type, const std::string& styleClass) {
+        return styles[std::to_string(type)+styleClass];
+    }
+
+    Styles::LayoutStyle::Ptr WidgetBuilder::getLayoutStyle(LayoutType type) {
+        return layoutStyles[type];
     }
 }
